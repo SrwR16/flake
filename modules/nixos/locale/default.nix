e9056838d7b324029d43287f23bac24b340532bf
SrwR16 <sarrwar16@gmail.com>
2025-07-04 07:59:09 +0200

    Update time zone setting to Asia/Dhaka in locale configuration

--- modules/nixos/locale/default.nix
+++ modules/nixos/locale/default.nix
@@ -1,6 +1,6 @@
 {
   time.hardwareClockInLocalTime = true;
-  time.timeZone = "Europe/Paris";
+  time.timeZone = "Asia/Dhaka";
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
