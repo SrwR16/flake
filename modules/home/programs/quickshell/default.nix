bc882a3fdf42b736cc2ec7dbc48ba6ea0560897b
SrwR16 <sarrwar16@gmail.com>
2025-07-29 06:31:08 +0600

    fix: uncomment quickshell package in configuration

--- modules/home/programs/quickshell/default.nix
+++ modules/home/programs/quickshell/default.nix
@@ -18,7 +18,7 @@ in
 
   config = mkIf cfg.enable {
     home.packages = [
-      # inputs.quickshell.packages."${pkgs.system}".quickshell
+      inputs.quickshell.packages."${pkgs.system}".quickshell
       inputs.caelestia-cli.packages."${pkgs.system}".default
       inputs.caelestia.packages."${pkgs.system}".default
     ];
